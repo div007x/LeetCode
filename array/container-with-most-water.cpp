c544a26e6b2e7f10c4fd242b39390cd7574fc771
Divakar R <divakar.codes@gmail.com>
2026-07-17 10:03:49 +0530

    LC-container-with-most-water [Runtime: N/A; Memory: N/A]

---
+++ array/container-with-most-water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        int left=0;
+        int right= height.size()-1;
+
+        int maxArea=1;
+
+        while(left< right){
+
+            int area= min(height[left],height[right]) * (right-left);
+
+            maxArea= max(maxArea, area);
+            if(height[left]<height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+
+        return maxArea;
+        
+    }
+};
