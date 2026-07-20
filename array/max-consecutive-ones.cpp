43a74c446b5a73eab21a605e89bf66f8bf6b3854
Divakar R <divakar.codes@gmail.com>
2026-07-20 18:58:11 +0530

    LC-max-consecutive-ones [Runtime: N/A; Memory: N/A]

---
+++ array/max-consecutive-ones.cpp
@@ -0,0 +1,20 @@
+#include<algorithm>
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+
+        int cnt=0,maxc=0;;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                cnt++;
+                maxc= max(cnt,maxc);
+            }else{
+
+                cnt=0;
+            }
+        }
+        return maxc;
+        
+    }
+};
