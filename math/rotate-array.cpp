c573d9bf70639c164588883bee86650fbf4764fc
Divakar R <divakar.codes@gmail.com>
2026-08-13 20:18:48 +0530

    LC-rotate-array [Runtime: N/A; Memory: N/A]

---
+++ math/rotate-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+void rotate(int start, int end, vector<int>& nums){
+    while(start<=end){
+        int temp= nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
+    }
+}
+    void rotate(vector<int>& nums, int k) {
+
+        int n= nums.size();
+        k = k%n;
+
+        rotate(0, n-k-1, nums);
+        rotate(n-k,n-1,nums);
+        rotate(0,n-1,nums);
+      
+        
+    }
+};
