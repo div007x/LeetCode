41241184612667c69893339e602b4d2a917004f5
Divakar R <divakar.codes@gmail.com>
2026-08-20 21:58:00 +0530

    LC-distribute-elements-into-two-arrays-i [Runtime: N/A; Memory: N/A]

--- array/distribute-elements-into-two-arrays-i.cpp
+++ array/distribute-elements-into-two-arrays-i.cpp
@@ -4,22 +4,23 @@ public:
         vector<int> arr1, arr2;
         int n= nums.size();
 
-        for(int i=0;i<n;i++){
-            if(i==0){
-                arr1.push_back(nums[0]);
-            }else if(i==1){
-                arr2.push_back(nums[1]);
-            }else{
-                if(arr1.back()> arr2.back()){
-                    arr1.push_back(nums[i]);
-                }else {
-                    arr2.push_back(nums[i]);
-                }
-
-            }    
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        for(int i=2;i<n;i++){
+            
+            if(arr1.back()> arr2.back()){
+                arr1.push_back(nums[i]);
+            }else {
+                arr2.push_back(nums[i]);
+            }
+ 
         }
 
-       arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+        for(int i=0;i<arr2.size();i++){
+            arr1.push_back(arr2[i]);
+        }
+
+    //    arr1.insert(arr1.end(),arr2.begin(),arr2.end());
 
        return arr1;
         
